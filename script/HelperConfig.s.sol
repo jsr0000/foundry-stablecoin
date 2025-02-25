222da88c17344ea46b66576b45c75c4d07e2ed8b
jsr0000 <josh.regnart@gmail.com>
2025-02-25 12:18:42 +0000

    network configs and makefile

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,7 +3,61 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "lib/forge-std/src/Script.sol";
+import {MockV3Aggregator} from "lib/chainlink-brownie-contracts/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
-    
+    uint8 public constant DECIMALS = 8;
+    uint256 public constant ETH_USD_PRICE = 2000e8;
+    uint256 public constant BTC_USD_PRICE = 1000e8;
+
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtsUsdPriecFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {}
+
+    function getSepoliaEthConfig()
+        public
+        view
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilEthConfig()
+        public
+        view
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        if (activeNetworkConfig != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            ETH_USD_PRICE
+        );
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            BTC_USD_PRICE
+        );
+        ERC20Mock wbtchMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+        vm.stopBroadcast();
+    }
 }
